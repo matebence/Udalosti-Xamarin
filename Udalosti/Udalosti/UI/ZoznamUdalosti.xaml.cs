332fac9cfd96dec814ee19df47364159a4afe42b
Bence Mate <m.bence05@gmail.com>
2018-08-26 00:14:00 -0800

    udalosti udaje bol zaimplementovany

---
+++ Udalosti/Udalosti/UI/ZoznamUdalosti.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Udalosti.Udalosti.UI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ZoznamUdalosti : ContentPage
+    {
+        public ZoznamUdalosti()
+        {
+            InitializeComponent();
+        }
+    }
+}
