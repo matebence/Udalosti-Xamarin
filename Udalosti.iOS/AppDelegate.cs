332094075b56f136159f7431620a02ef1cea373d
Bence <m.bence05@gmail.com>
2019-05-29 20:13:23 +0200

    odstranenie chyby pri vypytanie prav pre platformu iOS

--- Udalosti.iOS/AppDelegate.cs
+++ Udalosti.iOS/AppDelegate.cs
@@ -7,12 +7,13 @@ namespace Udalosti.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
+        CLLocationManager coreLocation;
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
-            var coreLocation = new CLLocationManager();
+            coreLocation = new CLLocationManager();
             coreLocation.RequestWhenInUseAuthorization();
 
             return base.FinishedLaunching(app, options);
